7735db320d6574ba16c716c8497a022683c50d9d
isurusiri <isuruanatomy@gmail.com>
2018-09-29 21:01:27 +0530

    comment extraction

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var parser = require('flow-parser')
 
 var fileReader = require('./utils/file-reader')
 
+var rx = require('rxjs');
+var rxOperators = require('rxjs/operators');
+
 var code  = fileReader.readFlowSync();
-console.log(code);
-console.log(JSON.stringify(parser.parse(code, {})));
+
+// strategy is to extract comments first along with their line numbers
+// then see if it has a method declaration next to it.
+var soruce = rx.from(parser.parse(code, {}).comments);
+var comments = soruce.pipe(rxOperators.map(comment => {
+    return {text: comment.value, line: comment.loc.start.line, methodSignature: ''}
+}));
+
+const subscription = comments.subscribe(val => console.log(val));
+
